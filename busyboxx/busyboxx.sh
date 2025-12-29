49d0e65ad281c288960cb3532e0bb658704dbf33
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2025-12-29 14:28:23 +0100

    feat: better init

--- busyboxx/busyboxx.sh
+++ busyboxx/busyboxx.sh
@@ -27,13 +27,85 @@ done
 cd ..
 
 echo '#!/bin/sh' > init
-echo 'mount -t sysfs sysfs /sys' >> init
-echo 'mount -t proc proc /proc' >> init
-echo 'mount -t devtmpfs udev /dev' >> init
-echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
-echo 'clear' >> init
-echo '/bin/sh' >> init
-echo 'poweroff -f' >> init
+echo '# Minimal but complete PID 1 init for BusyBox-based systems' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 1. Environment setup' > init
+echo '###############################################################################' > init
+echo 'export PATH=/sbin:/bin' > init
+echo 'export HOME=/root' > init
+echo 'export TERM=linux' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 2. Mount essential virtual filesystems' > init
+echo '###############################################################################' > init
+echo 'mount -t proc  proc  /proc' > init
+echo 'mount -t sysfs sysfs /sys' > init
+echo '' > init
+echo '# Device nodes' > init
+echo 'if ! mount -t devtmpfs devtmpfs /dev; then' > init
+echo '    mkdir -p /dev' > init
+echo '    mknod /dev/console c 5 1' > init
+echo '    mknod /dev/null    c 1 3' > init
+echo 'fi' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 3. Console setup' > init
+echo '###############################################################################' > init
+echo 'exec </dev/console >/dev/console 2>&1' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 4. Kernel log verbosity (optional)' > init
+echo '###############################################################################' > init
+echo 'sysctl -w kernel.printk="2 4 1 7" >/dev/null 2>&1' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 5. Signal handling and zombie reaping' > init
+echo '###############################################################################' > init
+echo 'trap 'reap' SIGCHLD' > init
+echo 'trap 'shutdown' SIGTERM SIGINT' > init
+echo '' > init
+echo 'reap() {' > init
+echo '    while wait -n 2>/dev/null; do :; done' > init
+echo '}' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 6. Optional: switch from initramfs to real root filesystem' > init
+echo '###############################################################################' > init
+echo '# Uncomment and adapt if you have a real rootfs' > init
+echo '#' > init
+echo '# mount /dev/sda1 /newroot || rescue_shell' > init
+echo '# exec switch_root /newroot /sbin/init' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 7. Emergency rescue shell' > init
+echo '###############################################################################' > init
+echo 'rescue_shell() {' > init
+echo '    echo "Entering emergency shell"' > init
+echo '    /bin/sh' > init
+echo '}' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 8. Shutdown handling' > init
+echo '###############################################################################' > init
+echo 'shutdown() {' > init
+echo '    echo "System shutting down..."' > init
+echo '    sync' > init
+echo '    poweroff -f' > init
+echo '}' > init
+echo '' > init
+echo '###############################################################################' > init
+echo '# 9. Process supervision (interactive shell)' > init
+echo '###############################################################################' > init
+echo 'echo "System initialized. Starting shell."' > init
+echo '' > init
+echo 'while true; do' > init
+echo '    /bin/sh' > init
+echo '    reap' > init
+echo '    echo "Shell exited. Restarting..."' > init
+echo '    sleep 1' > init
+echo 'done' > init
+echo '' > init
 
 
 chmod -R 777 .
--- busyboxx/start_qemu.sh
+++ busyboxx/start_qemu.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 -m 4096 -smp 6  -kernel bzImage -initrd initrd.img \
- -s -S #\
-# -append "console=ttyS0 nokaslr" -nographic
+qemu-system-x86_64 -m 4096 -smp 6  -kernel bzImage -initrd initrd.img -s -S -append "console=ttyS0 nokaslr" -nographic
